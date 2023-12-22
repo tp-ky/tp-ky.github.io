ae3e91fce3aafe6a6bf16341d78cc7cacba8e2ff
tp-ky <154560013+tp-ky@users.noreply.github.com>
2023-12-21 21:42:22 -0500

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// main.js
+
+// Optional smooth scrolling using JavaScript
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
